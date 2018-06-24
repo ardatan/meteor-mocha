e82bcddb650e5917a2822e57b905ad2c5e1e3c85
Arda TANRIKULU <ardatanrikulu@gmail.com>
2018-06-24 19:19:59 -0400

    Release 1.0.9

--- package.js
+++ package.js
@@ -3,7 +3,7 @@ Package.describe({
   summary: 'Run Meteor package or app tests with Mocha',
   git: 'https://github.com/ardatan/meteor-mocha.git',
   documentation: './README.md',
-  version: '1.0.8',
+  version: '1.0.9',
   testOnly: true,
 });
 
